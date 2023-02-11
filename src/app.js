a3e3544e550fd455dd690fe3a6f05c733d9ac44d
David Monroy <david2310.dmc@gmail.com>
2023-02-11 21:11:44 +0000

    Finalizado EndpointRegister sin refactorizar

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors")
+const register = require("./routes/register")
+require("./lib/MongooseConnection");
 
+const adverts = require("../initialAdverts")
+const users = require("../initialUser")
 //Configuraciones
 app.set("port", process.env.PORT || 3000);
 app.set("json spaces", 2);
 
 //Middleware
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/register",register)
+
+app.use(express.urlencoded({ extended: false }));
 
 //Nuestro primer WS Get
 app.get("/", (req, res) => {
-  res.json({
-    Title: "Hola mundo",
-  });
+  res.send("Welcome to the game online shop API...");
 });
+
+app.get("/adverts", (req,res)=>{
+  res.send(adverts)
+})
 
 //Iniciando el servidor
 app.listen(app.get("port"), () => {
